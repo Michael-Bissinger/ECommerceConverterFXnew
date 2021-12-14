c4b5fd5683a91cdc11657b5a86d03fbddab13f63
michael.bissinger <Michael.Bissinger@inb.de>
2021-12-14 16:49:47 +0100

    Date from "Conrad" is extracted and parsed for DATEV

--- src/ecommerce/converter/platformextractor/ExtractorConrad.java
+++ src/ecommerce/converter/platformextractor/ExtractorConrad.java
@@ -1,6 +1,8 @@
 package ecommerce.converter.platformextractor;
 
 import ecommerce.converter.extractortools.AccountWriter;
+import ecommerce.converter.extractortools.BroadcastCoordinator;
+import ecommerce.converter.extractortools.ItemPositionCoordinator;
 
 public class ExtractorConrad {
 
@@ -8,15 +10,14 @@ public class ExtractorConrad {
     private static Integer KONTO_KREDITOR = 7000166; // Kreditoren-Konto von "Conrad"
 
     private static String[] RELEVANTE_ITEMS = { // Relevante Items zur Berechnung
-            "", // 0
-            "", // 1
-            "", // 2
-            "", // 3
-            "", // 4
-            "", // 5
-            "", // 6
-            "", // 7
-            ""}; // 8
+            "Erstellungsdatum", // 0
+            "Bestellnr.", // 1
+            "Rechnungsnummer", // 2
+            "Transaktionsnummer", // 3
+            "Beschreibung", // 4
+            "Typ", // 5
+            "Betrag" // 6
+            };
 
     private static String[] GEBUEHRENARTEN = { // Mögliche Gebührenarten bei "Conrad"
             "Bezahlung Zusatzleistungen",
@@ -32,6 +33,10 @@ public class ExtractorConrad {
 
         String[][] daten_final = new String[rows-1][10];
 
+        // Positionen relevanter Items herausfinden
+        String[][] positionen = ItemPositionCoordinator.storeRelevantPositions(daten_original, columns, RELEVANTE_ITEMS);
+
+
         // ****************** DEBITORENKONTO ******************
         daten_final = AccountWriter.writeAccount(daten_final, rows, KONTO_DEBITOR, 3); // Schreibe Debitorenkonto  in 3. Reihe (also Position 2) von daten_final
         System.out.println("Fertig mit Debitoren-Konto!");
@@ -41,6 +46,8 @@ public class ExtractorConrad {
         daten_final = AccountWriter.writeAccount(daten_final, rows, KONTO_KREDITOR, 4); // Schreibe Kreditorenkonto  in 4. Reihe (also Position 3) von daten_final
         System.out.println("Fertig mit Kreditoren-Konto!");
 
+        // ****************** DATUM ******************
+        daten_final = BroadcastCoordinator.transferData(daten_final, positionen, daten_original, rows, RELEVANTE_ITEMS, RELEVANTE_ITEMS[0], 5);
 
 
 
--- src/ecommerce/converter/platformparser/ParserConrad.java
+++ src/ecommerce/converter/platformparser/ParserConrad.java
@@ -7,11 +7,11 @@ import java.text.ParseException;
 
 public class ParserConrad {
 
-    private static String DATUM_FORMAT = "dd-MM-yy - HH:mm:ss"; // Datumsformat von "Conrad" (01.08.19 - 15:20:53)
+    private static String DATUM_FORMAT = "dd.MM.yy - HH:mm:ss"; // Datumsformat von "Conrad" (01.08.19 - 15:20:53)
 
     public static String[][] parseConradData(String[][] daten) throws ParseException {
 
-        //daten = ParserDate.reformatDate(daten, DATUM_FORMAT);
+        daten = ParserDate.reformatDate(daten, DATUM_FORMAT);
 
         daten = ParserFix.writeFixation(daten);
 
